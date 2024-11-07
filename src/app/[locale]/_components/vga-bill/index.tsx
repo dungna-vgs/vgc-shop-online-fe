db524315f15aa40ac63b370ba80a7cd5c2c9ca18
dungna-vgs <dungna@vgsholding.com>
2024-11-07 19:14:59 +0700

    update UI

--- src/app/[locale]/_components/vga-bill/index.tsx
+++ src/app/[locale]/_components/vga-bill/index.tsx
@@ -29,12 +29,19 @@ export default function VGABill() {
             </p>
             <div>
               {!!vga ? (
-                <div className='flex p-4 gap-4 items-center rounded-[7px] border border-[#F1F1F1] bg-[#F6F6F6]'>
-                  <CardNumber vga={vga} showBuyButton={false} />
-
-                  <div className='flex flex-col font-semibold gap-4 justify-evenly items-center text-[16px]'>
-                    <span>VGA{vga.id}</span>
-                    <span>{formatCurrency(vga.amount)}đ</span>
+                <div className='flex justify-between px-3 rounded-[7px] border border-[#F1F1F1] bg-[#F6F6F6] font-semibold text-[16px] h-[90px]'>
+                  <div className='flex items-center'>
+                    <div className='m-[-60px]'>
+                      <CardNumber
+                        vga={vga}
+                        showBuyButton={false}
+                        className='transform scale-[0.4]'
+                      />
+                    </div>
+                    <p>VGA{vga.id}</p>
+                  </div>
+                  <div className='flex items-center'>
+                    <p>{formatCurrency(vga.amount)}đ</p>
                   </div>
                 </div>
               ) : !!feePackage ? (
